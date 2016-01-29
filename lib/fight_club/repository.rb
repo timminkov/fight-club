9f761a19beaa8591196db9de68b7ec6758cf3141
Tim Minkov <timothyminkov@gmail.com>
2016-01-28 17:51:10 -0800

    Add additional logging to clone

--- lib/fight_club/repository.rb
+++ lib/fight_club/repository.rb
@@ -4,9 +4,9 @@ module FightClub
       system("mkdir -p #{FightClub.working_dir}/#{name}") unless Dir.exists? "#{FightClub.working_dir}/#{name}"
 
       unless File.exists?("#{FightClub.working_dir}/#{name}/.gitconfig")
-        Logger.new(STDOUT).info "Cloning #{name}..."
-
+        Logger.new(STDOUT).info "Cloning using URI: #{uri} | Name: #{name} | Path: #{FightClub.working_dir}"
         git = Git.clone(uri, name, :path => FightClub.working_dir)
+        Logger.new(STDOUT).info "Clone complete!"
       else
         git = Git.open("#{FightClub.working_dir}/#{name}", :log => Logger.new(STDOUT))
       end
