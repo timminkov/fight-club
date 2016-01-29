658b8e7967e6a858be100ebedc6b1089aa47a281
Tim Minkov <timothyminkov@gmail.com>
2016-01-28 17:45:14 -0800

    Bump version and fix repository cloning

--- lib/fight_club/repository.rb
+++ lib/fight_club/repository.rb
@@ -3,12 +3,12 @@ module FightClub
     def update(uri, name)
       system("mkdir -p #{FightClub.working_dir}/#{name}") unless Dir.exists? "#{FightClub.working_dir}/#{name}"
 
-      git = Git.open("#{FightClub.working_dir}/#{name}", :log => Logger.new(STDOUT))
-
       unless File.exists?("#{FightClub.working_dir}/#{name}/.gitconfig")
         Logger.new(STDOUT).info "Cloning #{name}..."
 
-        git.clone(uri, name, :path => FightClub.working_dir)
+        git = Git.clone(uri, name, :path => FightClub.working_dir)
+      else
+        git = Git.open("#{FightClub.working_dir}/#{name}", :log => Logger.new(STDOUT))
       end
 
       git.fetch
